59ec7c0069f5bf7aae9c160074e8eae5acde4ef7
Aya <ayaaa.okzk@gmail.com>
2026-09-01 11:58:02 -0700

    Update the migration for cancellation fee

--- db/migrate/20260826215222_remove_payment_status_from_lessons.rb
+++ db/migrate/20260826215222_remove_payment_status_from_lessons.rb
@@ -1,6 +1,6 @@
 class RemovePaymentStatusFromLessons < ActiveRecord::Migration[8.1]
   def change
-    remove_column :lessons, :payment_status, :string, default: "unpaid"
-    remove_column :lessons, :cancellation_fee_currency, :string
+    remove_column :lessons, :payment_status, :string, default: "unpaid" if column_exists?(:lessons, :payment_status)
+    remove_column :lessons, :cancellation_fee_currency, :string if column_exists?(:lessons, :cancellation_fee_currency)
   end
 end
